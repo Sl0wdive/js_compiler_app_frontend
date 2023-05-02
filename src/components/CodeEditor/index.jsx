24ad78850dce30dad787000766a71aa6e0b95469
Sl0wdive <vladggobona@gmail.com>
2023-05-02 21:12:37 +0300

    CodeEditor onChange fix

--- src/components/CodeEditor/index.jsx
+++ src/components/CodeEditor/index.jsx
@@ -2,24 +2,25 @@ import React, { useState } from "react";
 
 import Editor from "@monaco-editor/react";
 
-const CodeEditor = ({ onChange, language, theme }) => {
-
-    const [text, setText] = React.useState('');
-  const onChangeF = React.useCallback((value) => {
-    setText(value);
-  }, []);
+const CodeEditor = ({ onChange, language, code, theme }) => {
+  const [value, setValue] = useState(code || "");
+  const onChangeF = (value) => {
+    setValue(value);
+    onChange("code", value);
+  };
 
   return (
-    <div className="overlay rounded-md overflow-hidden w-full h-full shadow-4xl">
+    <div className="overlay rounded-md overflow-hidden w-full h-full shadow-10xl">
       <Editor
         height="35vh"
         width={`50%`}
         language={"javascript"}
-        value={text} 
+        value={value} 
         theme={theme}
         defaultValue="console.log(1);"
         onChange={onChangeF}
       />
+      
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from "react";
 import CodeEditor from '../components/CodeEditor';
+import Button from '@mui/material/Button';
+
 
 function Home() {
   
-    
+  const javascriptDefault = `сonsole.log(32);`;
+  const [code, setCode] = useState(javascriptDefault);
+  const [theme] = useState('mytheme');
+
+  const OnClick = () => {
+    alert(code);
+  };
+
+  const onChange = (action, data) => {
+    switch (action) {
+      case "code": {
+        setCode(data);
+        break;
+      }
+      default: {
+        console.warn("Буває", action, data);
+      }
+    }
+  };
+
   return(
-    <CodeEditor/>
+    <>
+    <CodeEditor
+      code={code}
+      onChange={onChange}
+      theme={theme.value}
+    />
+    <Button onClick={OnClick} variant="contained">Алерт код</Button>
+    </>
   )
 }
 
