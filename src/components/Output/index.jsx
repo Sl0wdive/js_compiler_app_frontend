1425e02fd2f558ebc4da008f9f4c85c4fda33fb9
Sl0wdive <vladggobona@gmail.com>
2023-05-19 02:56:44 +0300

    if to switch case converted

--- src/components/Output/index.jsx
+++ src/components/Output/index.jsx
@@ -4,32 +4,33 @@ const Output = ({ outputDetails }) => {
     const getOutput = () => {
         let statusId = outputDetails?.status?.id;
     
-        if (statusId === 6) {
-          return (
-            <pre className="px-2 py-1 font-normal text-xs text-red-500">
-              {atob(outputDetails?.compile_output)}
-            </pre>
-          );
-        } else if (statusId === 3) {
-          return (
-            <pre className="px-2 py-1 font-normal text-xs text-green-500">
-              {atob(outputDetails.stdout) !== null
-                ? `${atob(outputDetails.stdout)}`
-                : null}
-            </pre>
-          );
-        } else if (statusId === 5) {
-          return (
-            <pre className="px-2 py-1 font-normal text-xs text-red-500">
-              {`Time Limit Exceeded`}
-            </pre>
-          );
-        } else {
-          return (
-            <pre className="px-2 py-1 font-normal text-xs text-red-500">
-              {atob(outputDetails?.stderr)}
-            </pre>
-          );
+        switch(statusId) {
+          case 6:
+            return (
+              <pre className="px-2 py-1 font-normal text-xs text-red-500">
+                {atob(outputDetails?.compile_output)}
+              </pre>
+            );
+          case 3:
+            return (
+              <pre className="px-2 py-1 font-normal text-xs text-green-500">
+                {atob(outputDetails.stdout) !== null
+                  ? `${atob(outputDetails.stdout)}`
+                  : null}
+              </pre>
+            );
+          case 5:
+            return (
+              <pre className="px-2 py-1 font-normal text-xs text-red-500">
+                {`Time Limit Exceeded`}
+              </pre>
+            );
+          default:
+            return (
+              <pre className="px-2 py-1 font-normal text-xs text-red-500">
+                {atob(outputDetails?.stderr)}
+              </pre>
+            );
         }
       };
       return (
